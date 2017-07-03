94a86e588085b3eca1e10842ee735bb520012545
xionggen <1907487869@qq.com>
2017-07-03 13:59:40 +0800

    添加输出重新commit

--- gitPro/src/gitPro/Hello.java
+++ gitPro/src/gitPro/Hello.java
@@ -2,6 +2,6 @@ package gitPro;
 
 public class Hello {
 	public static void main(String[] args) {
-		int remote = 1;
+		System.out.println("输出1");
 	}
 }
--- gitPro/src/gitPro/Hello2.java
+++ gitPro/src/gitPro/Hello2.java
@@ -2,6 +2,6 @@ package gitPro;
 
 public class Hello2 {
 	public static void main(String[] args) {
-		
+		System.out.println("输出2");
 	}
 }
